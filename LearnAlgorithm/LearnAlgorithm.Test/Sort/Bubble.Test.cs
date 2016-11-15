2c48435cf568a5564ec2041fd088c70dafc3e58e
jxw00100 <343275209@qq.com>
2016-11-15 17:42:10 +0800

    Bubble

---
+++ LearnAlgorithm/LearnAlgorithm.Test/Sort/Bubble.Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LearnAlgorithm;
+
+namespace LearnAlgorithm.Test
+{
+    [TestClass]
+    public class BubbleTest
+    {
+        private Int32[] _int32List = new[] { 7, 2, 33, 71, 256, 1, 31, 2, -1, 0, -22 };
+        private Sort.ISorter<Int32> _sorter = new Sort.Selection<Int32>();
+
+        [TestMethod]
+        public void NormalSort()
+        {
+            var list = _sorter.Sort(_int32List);
+
+            Assert.AreEqual(list[0], -22);
+            Assert.AreEqual(list[10], 256);
+            Assert.AreEqual(list.Count, 11);
+        }
+    }
+}
---
+++ LearnAlgorithm/LearnAlgorithm/Sort/Bubble.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using LearnAlgorithm.Assist;
+
+namespace LearnAlgorithm.Sort
+{
+    public class Bubble<T>:ISorter<T> where T : IComparable<T>
+    {
+        public IList<T> Sort(IList<T> list)
+        {
+            if (list == null) throw new ArgumentNullException();
+            IList<T> newList = list.CopyCollection();
+
+            for (int i = 0; i < newList.Count - 1; i++)
+            {
+                for (int j = newList.Count - 1; j > i; j--)
+                {
+                    if (newList[j].CompareTo(newList[j - 1]) < 0)
+                    {
+                        T temp = newList[j];
+                        newList[j] = newList[j - 1];
+                        newList[j - 1] = temp;
+                    }
+                }
+            }
+            
+            return newList;
+        }
+    }
+}
