c1ae0386e8be3438ae50479f9b4e2a06ebf3ebad
jxw00100 <343275209@qq.com>
2016-11-21 15:20:15 +0800

    Matrix Multiply

---
+++ LearnAlgorithm/LearnAlgorithm.Test/MatrixMultiplication/Straightfoward.Test.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel;
+using LearnAlgorithm.MatrixMultiplication;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LearnAlgorithm.Test.MatrixMultiplication
+{
+    [TestClass]
+    public class StraightfowardTest
+    {
+        IMatrixMuliplier<Double> _multiplier =  new Straightfoward<Double>();
+
+        private Double[,] _matrixA = new Double[,]
+        {
+            {3, -2, 5, 2, 1},
+            {4, 3, 22, 5, 3},
+            {1, 8, 3, 9, -3}
+        };
+
+        private Double[,] _matrixB = new Double[,]
+        {
+            {9, 3, -5, 3},
+            {-2, 1, 4, 10},
+            {5, 0, 25, 71},
+            {7, 12, -3, 4},
+            {2, 8, 99, 21}
+        };
+
+        [TestMethod]
+        public void Multiply()
+        {
+            var result = _multiplier.Multiply(_matrixA, _matrixB);
+
+            Assert.AreEqual(result[0, 0], 72);
+            Assert.AreEqual(result[0, 1], 39);
+            Assert.AreEqual(result[0, 2], 195);
+            Assert.AreEqual(result[0, 3], 373);
+
+            Assert.AreEqual(result[1, 0], 181);
+            Assert.AreEqual(result[1, 1], 99);
+            Assert.AreEqual(result[1, 2], 824);
+            Assert.AreEqual(result[1, 3], 1687);
+
+            Assert.AreEqual(result[2, 0], 65);
+            Assert.AreEqual(result[2, 1], 95);
+            Assert.AreEqual(result[2, 2], -222);
+            Assert.AreEqual(result[2, 3], 269);
+        }
+    }
+}
---
+++ LearnAlgorithm/LearnAlgorithm/MatrixMultiplication/Straightfoward.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LearnAlgorithm.MatrixMultiplication
+{
+    /*
+     * Θ(n3)
+     */
+    public class Straightfoward<T>:IMatrixMuliplier<T> where T: struct
+    {
+        public T[,] Multiply(T[,] matrixA, T[,] matrixB)
+        {
+            Int32 rowsCountA = matrixA.GetLength(0), 
+                rowsCountB = matrixB.GetLength(0), 
+                columnsCountA = matrixA.GetLength(1), 
+                columnsCountB = matrixB.GetLength(1);
+
+            if(columnsCountA != rowsCountB) throw new ArgumentException("Matrix size between A and B are not match");
+
+            T[,] result = new T[rowsCountA, columnsCountB];
+
+            for (var i = 0; i < rowsCountA; i++)
+            {
+                for (var j = 0; j < columnsCountB; j++)
+                {
+                    dynamic sum = 0;
+                    for (var k = 0; k < columnsCountA; k++)
+                    {
+                        sum += (dynamic) matrixA[i, k]*matrixB[k, j];
+                    }
+                    result[i, j] = sum;
+                }
+            }
+
+            return result;
+        }
+    }
+}
